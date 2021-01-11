830fd2d3ba42f80d7ec3c516088fda3a44754c45
Peter Gemmell <petergemmell65@hotmail.com>
2021-01-11 13:58:15 +0000

    defined interface and defined interaction between os and interface

--- MyPlayer.py
+++ MyPlayer.py
@@ -2,3 +2,11 @@ import pygame #used to create video games, music players etc
 import tkinter as tkr # used to develop a GUI
 from tkinter.filedialog import askdirectory #it permit to select dir
 import os #it permits to interact with the operating system
+
+music_player = tkr.Tk()
+music_player.title("Life In Music")
+music_player.geometry("450x350")
+
+directory = askdirectory()
+os.chdir(directory) # This permits to change the current directory
+song_list = os.listdir() # This returns the list of files songs
