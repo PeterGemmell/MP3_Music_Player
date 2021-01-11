cebd1737f3a591db95994c20dc85ef54be8741ed
Peter Gemmell <petergemmell65@hotmail.com>
2021-01-11 14:45:40 +0000

    buttons for the interface defined

--- MyPlayer.py
+++ MyPlayer.py
@@ -42,3 +42,11 @@ def pause():
 
 def unpause():
     pygame.mixer.music.unpause()
+
+
+# Next we create the buttons for the interface of the music player
+
+Button1 = tkr.Button(music_player, width=5, height=3,font="Helvetica 12 bold", text="PLAY", command=play, bg="blue",fg="white")
+Button2 = tkr.Button(music_player, width=5, height=3, font=”Helvetica 12 bold”, text=”STOP”, command=stop, bg=”red”, fg=”white”)
+Button3 = tkr.Button(music_player, width=5, height=3, font=”Helvetica 12 bold”, text=”PAUSE”, command=pause, bg=”purple”, fg=”white”)
+Button4 = tkr.Button(music_player, width=5, height=3, font=”Helvetica 12 bold”, text=”UNPAUSE”, command=unpause, bg=”orange”, fg=”white”)
