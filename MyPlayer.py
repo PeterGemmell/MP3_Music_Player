72da44af796009b073d58b37bd91652be0e13538
Peter Gemmell <petergemmell65@hotmail.com>
2021-01-11 13:51:53 +0000

    imports done

---
+++ MyPlayer.py
@@ -0,0 +1,4 @@
+import pygame #used to create video games, music players etc
+import tkinter as tkr # used to develop a GUI
+from tkinter.filedialog import askdirectory #it permit to select dir
+import os #it permits to interact with the operating system
