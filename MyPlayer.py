9e37820e3a05b7971705fddcbb5ba14379db49c4
Peter Gemmell <petergemmell65@hotmail.com>
2021-01-11 14:10:35 +0000

    defined play list variable and for loop for adding songs to list box

--- MyPlayer.py
+++ MyPlayer.py
@@ -10,3 +10,14 @@ music_player.geometry("450x350")
 directory = askdirectory()
 os.chdir(directory) # This permits to change the current directory
 song_list = os.listdir() # This returns the list of files songs
+
+# Here we create a variable which brings the interface to display items to the user.
+# We then create a for loop in order to push the programme to select each item from the song list and
+# insert them into the list box.
+play_list = tkr.Listbox(music_player, font="Helvetica 12 bold",
+bg="yellow", selectmode=tkr.SINGLE)
+
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
